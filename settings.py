de38abcc5df7d1bd6cd2c6a3898d93566f6b68d6
Joao Tiago Aparicio <joaotiagoaparicio@gmail.com>
2020-04-30 07:25:30 +0100

    settings for the project created

---
+++ settings.py
@@ -0,0 +1,31 @@
+############
+# SETTINGS #
+############
+
+simulation_time = 20
+nr_vehicles = 1
+nr_stations = 1
+nr_priority_vehicles = 0
+nr_disasters = 0
+
+
+#map
+point = (38.736828, -9.138222) # IST
+
+place = 'Alameda_buildings'
+network_type='drive'
+bldg_color='grey'
+fig_height = 6
+dpi=93
+dist=1000
+default_width=4
+street_widths = {'footway' : 0.5,
+                'steps' : 0.5,
+                'pedestrian' : 0.5,
+                'path' : 0.5,
+                'track' : 0.5,
+                'service' : 2,
+                'residential' : 3,
+                'primary' : 5,
+                'motorway' : 6}
+
