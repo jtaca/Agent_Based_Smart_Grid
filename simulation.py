282e4502ecbe437c3adb6e85a4d868906a934b1a
Joao Tiago Aparicio <joaotiagoaparicio@gmail.com>
2020-05-19 12:30:42 +0100

    simulation to semi test features done
    and working :P

--- simulation.py
+++ simulation.py
@@ -1,1 +1,12 @@
 import settings 
+import map
+from agents import charger_handler,driver_assistant,energy_broker,power_operative, geographic_agent
+
+map1 = map.map()
+
+b = geographic_agent.geographic_agent(38.7414116,-9.143627785022142)
+print(b.get_latitude())
+a = energy_broker.energy_broker(38.7414116,-9.143627785022142)
+print(a.get_latitude())
+
+print(a.get_closest_node(map1.get_map()))
