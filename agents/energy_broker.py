4c4dabb83b40de655275361d6a66076415612ba8
Joao Tiago Aparicio <joaotiagoaparicio@gmail.com>
2020-05-20 22:37:39 +0100

    Update energy_broker.py

--- agents/energy_broker.py
+++ agents/energy_broker.py
@@ -3,6 +3,6 @@ import agents.geographic_agent
 
 class energy_broker(agents.geographic_agent.geographic_agent):
 
-        def __init__(self,lat,lng):
-            agents.geographic_agent.geographic_agent.__init__(self,lat,lng)
-            self.name = "energy broker"
+    def __init__(self,lat,lng):
+        agents.geographic_agent.geographic_agent.__init__(self,lat,lng)
+        self.name = "energy broker"
