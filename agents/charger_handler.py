044b3dc9c8d66a27816c0a36cdf7ab2b363859d7
Joao Tiago Aparicio <joaotiagoaparicio@gmail.com>
2020-05-21 03:02:17 +0100

    Update charger_handler.py

--- agents/charger_handler.py
+++ agents/charger_handler.py
@@ -4,7 +4,8 @@ import agents.geographic_agent
 class charger_handler(agents.geographic_agent.geographic_agent):
 
 	def __init__(self,lat,lng, map):
-		agents.geographic_agent.geographic_agent.__init__(self,lat,lng, G)
+		agents.geographic_agent.geographic_agent.__init__(self,lat,lng)
 		self.name = "charger handler"
 		self.map = map
+
 
